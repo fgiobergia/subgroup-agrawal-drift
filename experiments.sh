ca129419087eee436937f893f96e798836171172
fgiobergia <flavio.giobergia@gmail.com>
2024-06-11 17:11:58 +0200

    minor fixes

--- experiments.sh
+++ experiments.sh
@@ -6,5 +6,5 @@ n_steps=20
 
 for sup_size in $(seq -2.0 0.1 0.0); do
     sup=$(python -c "print(10.0 ** $sup_size)")
-    python run.py --sg-size=$sup --models-file=best_config.json --perturbation=0.1
+    python run.py --sg-size=$sup --models-file=best_config.json --perturbation=0.25
 done
