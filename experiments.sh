163ef97d96ae62fa58ee06e15106e811cc077a25
fgiobergia <flavio.giobergia@gmail.com>
2024-06-11 16:05:14 +0200

    stuff

---
+++ experiments.sh
@@ -0,0 +1,10 @@
+
+start_exp=-2.0 # -2!!!
+end_exp=0.0
+step=0.1
+n_steps=20
+
+for sup_size in $(seq -2.0 0.1 0.0); do
+    sup=$(python -c "print(10.0 ** $sup_size)")
+    python run.py --sg-size=$sup --models-file=best_config.json --perturbation=0.1
+done
--- run.py
+++ run.py
@@ -7,6 +7,9 @@ import argparse
 from river.drift import binary as drift
 from sklearn.model_selection import ParameterGrid
 import json
+import pickle
+import os
+from tqdm import tqdm
 
 
 def to_Xy(ds, n : int = 10_000, drift_info : bool = False):
@@ -36,32 +39,24 @@ if __name__ == "__main__":
     argparser.add_argument("--seed", type=int, default=42)
     argparser.add_argument("--sg-size", type=float, default=0.1)
     argparser.add_argument("--perturbation", type=float, default=0.05)
+    argparser.add_argument("--n-runs", type=int, default=10)
+    argparser.add_argument("--models-file", type=str)
+
     args = argparser.parse_args()
+    print(args)
 
     # set seed
     np.random.seed(args.seed)
 
-    models = {
-        "DDM": (drift.DDM, {
-            "warm_start": [ 10, 50, 100, 500, 1000, 5000 ],
-            "drift_threshold": [ 0.1, 0.5, 1, 3, 5, 10]
-        }),
-        "EDDM": (drift.EDDM, {
-            "warm_start": [ 10, 50, 100, 500, 1000, 5000 ],
-            "beta": np.arange(0.0, 1.1, 0.1),
-            "alpha": [1.],
-        }),
-        "FHDDM": (drift.FHDDM, {
-            "sliding_window_size": [10, 50, 100, 500, 1000, 5000],
-            "confidence_level": [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
-        }),
-        "HDDM_A": (drift.HDDM_A, {
-            "drift_confidence": [1e-4, 1e-3, 1e-2, 1e-1],
-        }),
-        "HDDM_W": (drift.HDDM_W, {
-            "drift_confidence": [1e-4, 1e-3, 1e-2, 1e-1],
-            "lambda_val": [ 0.01, 0.05, 0.1, 0.5 ],
-        })
+    with open(args.models_file) as f:
+        models = json.load(f)
+
+    model_lookup = {
+        "DDM": drift.DDM,
+        "EDDM": drift.EDDM,
+        "FHDDM": drift.FHDDM,
+        "HDDM_A": drift.HDDM_A,
+        "HDDM_W": drift.HDDM_W
     }
 
     train_size = 10_000
@@ -71,52 +66,61 @@ if __name__ == "__main__":
     sg_size = args.sg_size
     perturbation = args.perturbation
     n_runs = 10
+    outfile = f"results/sg_{sg_size}_pert_{perturbation}_nruns_{n_runs}.pkl"
+
+    if os.path.isfile(outfile):
+        print("File exists, skipping")
+        exit()
+
 
     results = {}
 
-    for model_name, (model_func, params) in models.items():
+    for model_name, params in models.items():
         print(model_name)
+        model_func = model_lookup[model_name]
 
         for config in ParameterGrid(params):
+            print(config)
 
-            key = f"{model_name}|{config}"
+            key = (model_name, str(config))
             results[key] = {
                 "y_pred": [],
                 "y_true": [],
             }
-            for run in range(n_runs):
-                for with_drift in [True, False]:
-                    ds = SubgroupDriftAgrawal(sg_size=sg_size if with_drift else 0.0,
-                                              perturbation=perturbation,
-                                              position=train_size + (test_batches // 2) * batch_size,
-                                              width = (test_batches // 4) * batch_size)
+            with tqdm(range(n_runs)) as bar:
+                for run in bar:
+                    for with_drift in [True, False]:
+                        ds = SubgroupDriftAgrawal(sg_size=sg_size if with_drift else 0.0,
+                                                perturbation=perturbation,
+                                                position=train_size + (test_batches // 2) * batch_size,
+                                                width = (test_batches // 4) * batch_size)
 
-                    X_train, y_train = to_Xy(ds, train_size)
+                        X_train, y_train = to_Xy(ds, train_size)
 
-                    clf = DecisionTreeClassifier(max_depth=3)
-                    clf.fit(X_train, y_train)
+                        clf = DecisionTreeClassifier(max_depth=3)
+                        clf.fit(X_train, y_train)
 
-                    detector = model_func(**config)
+                        detector = model_func(**config)
 
-                    detections = []
-                    pos = 0 # position in the stream
-                    for batch in range(test_batches):
-                        X_test, y_test, is_sg, _ = to_Xy(ds, batch_size, drift_info=True)
-                        y_pred = clf.predict(X_test)
+                        detections = []
+                        pos = 0 # position in the stream
+                        for batch in range(test_batches):
+                            X_test, y_test, is_sg, _ = to_Xy(ds, batch_size, drift_info=True)
+                            y_pred = clf.predict(X_test)
 
-                        # 0 : correct
-                        # 1 : error
-                        for p in y_test != y_pred:
-                            detector.update(p)
+                            # 0 : correct
+                            # 1 : error
+                            for p in y_test != y_pred:
+                                detector.update(p)
 
-                            if batch > bootstrap and detector.drift_detected:
-                                detections.append(pos)
-                            
-                            pos += 1
+                                if batch > bootstrap and detector.drift_detected:
+                                    detections.append(pos)
+                                
+                                pos += 1
 
-                    results[key]["y_pred"].append(len(detections))
-                    results[key]["y_true"].append(with_drift)
-                    
-                    # store json
-                    with open(f"results.json", "w") as f:
-                        json.dump(results, f)
+                        results[key]["y_pred"].append(len(detections))
+                        results[key]["y_true"].append(with_drift)
+                        
+                        # store json
+                        with open(outfile, "wb") as f:
+                            pickle.dump(results, f)
